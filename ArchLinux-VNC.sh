d5c18c988fd3626de551d5755c0346836a48ddf4
ItsChanGat <youness-changat@hotmail.com>
2025-02-09 16:15:24 +0100

    Update ArchLinux-VNC.sh

--- ArchLinux-VNC.sh
+++ ArchLinux-VNC.sh
@@ -3,7 +3,7 @@ echo "Click Web Preview > Preview On Port 6080 to access noVNC RDP"
 echo "Installing Arch Linux Environment + VNC Server (1-2 minutes)...."
 
 # تشغيل الحاوية في الخلفية مع إعدادات VNC
-sudo docker run --name archlinux-vnc --privileged --shm-size 1g -d -p 5900:5900 -p 6080:6080 -e VNC_PASSWD=password dcsunset/archlinux-vnc
+sudo docker run --name archlinux-vnc --privileged --shm-size 1g -d -p 5900:5900 -p 6080:6080 -e VNC_PASSWD=Jor3a-Ti9niya dcsunset/archlinux-vnc
 
 # تشغيل عداد زمني لمدة 12 ساعة (43200 ثانية)
 seq 1 43200 | while read i; do 
